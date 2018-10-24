46623374d70210e138daec2a705f571aa53646a0
jeps <petteri.stenius@ubisecure.com>
2018-10-24 16:55:21 +0300

    update

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleSPA.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Redirect("~/spa.html");
+        }
+    }
+}
