f73d19eb51073f3ee9286e6e31f632b8fea0b734
jeps <petteri.stenius@ubisecure.com>
2018-11-16 17:16:36 +0200

    move wwwroot -> docs

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace SimpleSPA
 {
@@ -32,7 +34,10 @@ namespace SimpleSPA
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "docs"))
+            });
 
             app.UseMvcWithDefaultRoute();
         }
